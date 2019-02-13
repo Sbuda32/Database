84f2efa092d6dbe1baef047efa78ee7fb6139f27
Sbuda32 <41370353+Sbuda32@users.noreply.github.com>
2019-02-13 16:17:07 +0200

    Update sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE Customer( 
+  CustomerID INT auto_increment, 
+  FirstName VARCHAR(100) NOT NULL, 
+  LastName VARCHAR(100) NOT NULL, 
+  Gender VARCHAR(6) NOT NULL, 
+  Address TEXT, 
+  Phone INT(10), 
+  Email VARCHAR(100), 
+  City VARCHAR(100), 
+  Country VARCHAR(100), 
+  PRIMARY KEY(CustomerID)
+);
