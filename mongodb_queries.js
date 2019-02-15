41783e4425f7fd6d4961e2c4bf9a1a4eab671ea0
Sbuda32 <41370353+Sbuda32@users.noreply.github.com>
2019-02-15 16:46:14 +0200

    Update mongodb_queries.js

--- mongodb_queries.js
+++ mongodb_queries.js
@@ -4,12 +4,12 @@ use STORE
 #Query to create a new collection
 db.createCollection('MOVIE')
 
-#Query to add fields to our document in the collection i've created above
-db.movie.insert( {   
-  movieTitle: 'The Avatar',   
-  genre: 'Sci-Fi',   
-  releaseYear: 2009,   
-  movieLanguage: 'English',   
-  movieLength: '2:45:00' 
+#Query to add fields to our document in the collection i have created above
+db.movie.insert( {
+  movieTitle: 'The Avatar',
+  genre: 'Sci-Fi',
+  releaseYear: 2009,
+  movieLanguage: 'English',
+  movieLength: '2:45:01' 
 })
 
