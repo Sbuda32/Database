1bf4981900019e87b1f6f0bbac8472a362bf363f
Sbuda32 <41370353+Sbuda32@users.noreply.github.com>
2019-02-15 16:43:41 +0200

    Added two queries

--- mongodb_queries.js
+++ mongodb_queries.js
@@ -1,4 +1,15 @@
 #Query to create a database named STORE
 use STORE
 
+#Query to create a new collection
+db.createCollection('MOVIE')
+
+#Query to add fields to our document in the collection i've created above
+db.movie.insert( {   
+  movieTitle: 'The Avatar',   
+  genre: 'Sci-Fi',   
+  releaseYear: 2009,   
+  movieLanguage: 'English',   
+  movieLength: '2:45:00' 
+})
 
